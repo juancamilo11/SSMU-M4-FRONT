59ce3f31c450c2396d8780a7ff995f20548f79f0
Juan Camilo Cardona Calderón <juancamilo19997814@gmail.com>
2023-11-21 00:38:53 -0500

    Se cambia a aws

--- next.config.js
+++ next.config.js
@@ -4,9 +4,9 @@ const nextConfig = {
   reactStrictMode: true,
   output: 'export',
   env: {
-    NEXT_PUBLIC_BACKEND_SERVER_URL: 'http://localhost:8080/api/v1',
-    // NEXT_PUBLIC_BACKEND_SERVER_URL:
-    //   'http://ec2-54-210-220-33.compute-1.amazonaws.com:8080/api/v1',
+    // NEXT_PUBLIC_BACKEND_SERVER_URL: 'http://localhost:8080/api/v1',
+    NEXT_PUBLIC_BACKEND_SERVER_URL:
+      'http://ec2-54-210-220-33.compute-1.amazonaws.com:8080/api/v1',
   },
 };
 
